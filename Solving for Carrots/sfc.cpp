5389d09a173cb7a0438d6de590ca72b643b9c0f3
Evan-Crowley <evanjcrowley@gmail.com>
2019-02-23 00:08:46 -0700

    added carrots

---
+++ Solving for Carrots/sfc.cpp
@@ -0,0 +1,16 @@
+/*
+Evan Crowley
+Kattis - Solving for Carrots
+https://open.kattis.com/problems/carrots
+*/
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, p;
+    cin >> n >> p;
+    cout << p;
+    return 0;
+}
