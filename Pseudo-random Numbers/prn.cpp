ec15760cb2548069cc96b6ff9853f58596d5c9bf
Evan Crowley <evanjcrowley@gmail.com>
2019-02-23 00:50:47 -0700

    added pseudo-random numbers

---
+++ Pseudo-random Numbers/prn.cpp
@@ -0,0 +1,17 @@
+/*
+Evan Crowley
+2/23/19
+Kattis - Pseudo-random Numbers
+https://open.kattis.com/problems/pseudorandomnumbers
+*/
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+
+
+    return 0;
+}
