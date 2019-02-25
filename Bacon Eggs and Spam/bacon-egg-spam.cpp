0f1dc693165548aa0ea5997913a77e71e5b9fb94
Evan-Crowley <evanjcrowley@gmail.com>
2019-02-25 09:48:24 -0700

    added bacon eggs and spam

---
+++ Bacon Eggs and Spam/bacon-egg-spam.cpp
@@ -0,0 +1,18 @@
+/*
+Evan Crowley
+2/25/19
+Kattis - Bacon, Eggs, and Spam
+https://open.kattis.com/problems/baconeggsandspam
+*/
+#include <iostream>
+#include <iomanip>
+#include <cstring>
+#include <sstream>
+
+using namespace std;
+
+int main(int argc, char * argv[])
+{
+
+    return 0;
+}
